40031d21ee8758d543a797295bc3a80aaf4ab7c4
bkoi <bernadettkoi@gmail.com>
2022-01-29 11:51:15 +0000

    Add new about view to run.py, fix image links in company.json

--- run.py
+++ run.py
@@ -14,9 +14,19 @@ def index():
 @app.route("/about")
 def about():
     data = []
-    with open("data/company.json", "r") as json-data:
+    with open("data/company.json", "r") as json_data:
         data = json.load(json_data)
     return render_template("about.html", page_title="About", company=data)
+
+@app.route("/about/<member_name>")
+def about_member(member_name):
+    member = {}
+    with open("data/company.json", "r") as json_data:
+        data = json.load(json_data)
+        for obj in data:
+            if obj["url"] == member_name:
+                member = obj
+    return "<h1>" + member["name"] + "</h1>"
 
 
 @app.route("/contact")
